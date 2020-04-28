d6d31ce7a8dcb34f7d775ae0f2adbe8b44aa14d3
AdamGibbs <adam-gibbs@outlook.com>
2020-04-28 19:37:03 +0100

    Added loading bars for ingest

--- ingest.py
+++ ingest.py
@@ -1,23 +1,39 @@
 def LoadNoDupes(path):
     wordList = []
+    count = 0
 
     file = open(path,"r")
 
     for word in file:
+        count += 1
         word = word.upper()
         word = word.strip()
         if word not in wordList:
             wordList.append(word)
+
+        if count % 100 == 0:
+            print(".", end="")
+
+        if count % 1000 == 0:
+           print(" +", word, "\n(", count, ") + ", end = "")
 
     print("\n")
     return wordList
 
 def GetInfo(inList):
     infoList = []
+    count = 0
 
     for word in inList:
+        count += 1
         wordInfo = [word, len(word), uniqueChars(word)]
         infoList.append(wordInfo)
+
+        if count % 100 == 0:
+            print("-", end="")
+
+        if count % 1000 == 0:
+            print(" +", word, "\n(", count, ") + ", end = "")
 
     return infoList
 
